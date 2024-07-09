cc3c984764665c5e851386dc79bbc883819b2ca9
shiv2130 <shivanahuomili@gmail.com>
2024-07-09 23:29:43 +0530

    second commit this is looping

---
+++ loop.java
@@ -0,0 +1,23 @@
+
+
+public class loop {
+    public static void main(String[] args) {
+        // for(int i=1; i<12; i++){
+        //     System.out.println(i);
+        // }
+        // int i=1;
+        // while (i<11) {
+        //     System.out.println(i);
+        //     i=i+1;
+        // }
+        int i=1;
+        // while (i<11) { 
+        //     System.out.println("Shivanshu");
+        //     i= i+1;
+        // }
+        do { 
+            System.out.println("Hello World!");
+            
+        } while (i<11);
+    }
+}
---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sum {
+   public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+       int n=sc.nextInt();
+       for(int i=0; i<11; i++){
+           
+           System.out.println(i*n);
+       }
+   } 
+}
