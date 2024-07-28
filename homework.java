0858f9359cd3b2a03af68d6aaed1bac6ee95f5e1
shiv2130 <shivanahuomili@gmail.com>
2024-07-28 16:50:25 +0530

    just some practice done

--- homework.java
+++ homework.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 
 public class homework {
-    public static void primeNumbers(int a){
-        if(a % 2!=0){
-            System.out.println("Prime Number");
-        }else {
-            System.out.println("Not a prime number");
-        }
+    // public static void primeNumbers(int a){
+    //     if(a % 2!=0){
+    //         System.out.println("Prime Number");
+    //     }else {
+    //         System.out.println("Not a prime number");
+    //     }
+    // }
+    public static void evenNumber(int a){
+       for(int i=1; i<=10; i++){
+         int b=i*a;
+         System.out.println(b);
+       }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        primeNumbers(n);
+        evenNumber(n);
     }
 }
