8c1d12702734d4998457e0f1baabfb948a5702b0
shiv2130 <shivanahuomili@gmail.com>
2024-07-31 16:23:12 +0530

    Made a program for calculation of powers

---
+++ practice2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class practice2 {
+    public static void avg(int a, int b) {
+        System.out.println(Math.pow(a, b));
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        avg(n, m);
+
+    }
+}
